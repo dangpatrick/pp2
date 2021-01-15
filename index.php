18f99cb8e6d399c71ca3c572e427470aa85494cd
Patrick Dang <pdang2@mail.greenriver.edu>
2021-01-15 15:19:15 -0500

    created index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/* Name: Patrick Dang & George McMullen
+ * Date: 1/15/2021
+ *
+ */
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+</body>
+</html>
+
+<?php
+echo "1. PHP Array Practice";
