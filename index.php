f0f1466ac6fb1f38e01e49ab49c3ea3e74cc29a2
Patrick Dang <pdang2@mail.greenriver.edu>
2021-01-16 00:25:35 -0500

    test largest function - returns largest number in an array

--- index.php
+++ index.php
@@ -23,13 +23,18 @@
 
 <?php
 
-echo "Step 1: <br>";
+echo "Step 1: Create an index.php file in your pp2 directory and give it a title and header Pair Program 2<br>";
 echo "PHP Array Practice";
-echo "<br><br>Step 2: <br>";
+echo "<br><br>Step 2: Define an array numbers, which contains 7, 9, 8, 9, 8, 8, 6. In the same PHP block, write a function printArr() that takes an array as a parameter and prints the array, one item per line. Call your function and pass it the number array. Test your code and then commit your changes.<br>";
 $numbers = array(7, 9, 8, 9, 8, 8, 6);
 include "functions.php";
 
 printArr($numbers);
 
 echo "<br><br>Step 3: Moved print function into an include file called functions.php in the same directory<br>";
+echo "<br><br>Step 4: In your functions file, define a function called largest() that takes an array as a parameter 
+and returns the largest value in the array. Test the function in your index page, and then commit your changes. <br>";
+echo "Largest Number <br>";
+echo largest($numbers);
+
 
