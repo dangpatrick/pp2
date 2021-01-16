87f5a2678056033f2f0b03b5d4a46f2c9e5a6f15
Patrick Dang <pdang2@mail.greenriver.edu>
2021-01-16 00:04:54 -0500

    Step 2: Define an array  that takes an array as parameter & prints array, one item per line

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
+
 <?php
 /* Name: Patrick Dang & George McMullen
  * Date: 1/15/2021
- *
+ * File URL: http://pdang.greenriverdev.com/328/pp2/index.php
  */
 
 // Turn on error reporting
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//error_reporting(E_ALL);
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Pair Program 2</title>
 </head>
 <body>
 
@@ -21,4 +22,17 @@ error_reporting(E_ALL);
 </html>
 
 <?php
-echo "1. PHP Array Practice";
+
+function printArr($myArr)
+{
+    foreach ($myArr as $item)
+    {
+        echo $item . "<br>";
+    }
+}
+echo "Step 1: <br>";
+echo "PHP Array Practice";
+echo "<br><br>Step 2: <br>";
+$numbers = array(7, 9, 8, 9, 8, 8, 6);
+include  "";
+printArr($numbers);
